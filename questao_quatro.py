ea29b99a26f5e1cbe85587f1deeaac05a25bb5ce
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-03-14 23:22:39 -0300

    :sparkles: feat: Desafio quatro

---
+++ questao_quatro.py
@@ -0,0 +1,1 @@
+# Ligo o interruptor e deixarei ligado por um tempo. Depois disso, irei identificar a temperatura tocando nas lâmpadas. Se a lâmpada estiver quente, significa que o interruptor que liguei corresponde a tal lâmpada. Porém, se a lâmpada estiver fria, significa que o interruptor que eu não liguei está conectado a essa lâmpada.
