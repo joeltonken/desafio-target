909c01784821c8ba40589cfd84d9c82571485d93
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-03-14 23:13:51 -0300

    :sparkles: feat: Desafio tres

---
+++ questao_tres.py
@@ -0,0 +1,11 @@
+# a) 1, 3, 5, 7, 9 --> Lógica: Número anterior + 2
+
+# b) 2, 4, 8, 16, 32, 64, 128 --> Lógica: Número anterior * 2
+
+# c) 0, 1, 4, 9, 16, 25, 36, 49 --> Lógica: O quadrados dos números... 0, 1, 2^2, 3^2 [...]
+
+# d) 4, 16, 36, 64, 100 --> Lógica: Cada número é igual ao quadrado dos números pares. 
+
+# e) 1, 1, 2, 3, 5, 8, 13 --> Lógica: Esta é a sequência de Fibonacci, onde cada número é a soma dos dois anteriores.
+
+# f) 2,10, 12, 16, 17, 18, 19, 200 --> Lógica: Números com a inicial 'D'
